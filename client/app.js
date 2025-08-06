003f6802d5a43328c8d4ffaec514729452e673a2
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-06 11:59:05 +0200

    add reference in app.js

---
+++ client/app.js
@@ -0,0 +1,6 @@
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection = document.querySelector('#messages-section');
+const messagesList = document.querySelector('#messages-section__list');
+const addMessageForm = document.querySelector('#add-message-form');
+const userNameInput = document.querySelector('#username');
+const messageContentInput = document.querySelector('#message-content');
